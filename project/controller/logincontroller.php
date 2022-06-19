c7b1338cf46e168ae46a3b3d6380cd2fb8c552d2
uchsas <44012824+uchsas@users.noreply.github.com>
2022-06-19 22:10:36 +0600

    Update logincontroller.php

--- project/controller/logincontroller.php
+++ project/controller/logincontroller.php
@@ -1,4 +1,42 @@
-<?php
-    
-		
-?>
+<?php 
+
+		$nameErr = $passworderr = $emailErr = $genderErr = "" ;
+    	$Password = $Username =  $Email = $gender "";
+
+    	
+	    	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["Username"])) {
+    $nameErr = "Name is required";
+  } else {
+    $Username = test_input($_POST["Username"]);
+
+    if (!preg_match("/^[a-zA-Z-' ]*$/",$Username)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+    		
+
+			if(empty($_POST["password"]))
+    		{
+    		$passworderr = "*Insert password";
+    		} 
+    		else
+    		{
+    			$password = ($_POST["password"]);
+    			if(strlen($password)<8) 
+    			{
+           		 $passworderr = "Your Password Must Contain At Least 8 Characters!";
+       			}
+       			elseif (!preg_match("/[@,#,$,%]/",$password)) 
+       			{
+       				$passworderr = "*Minimum One Special Character";
+       				
+       			}
+    		}
+
+
+
+		}
+
+
+	 ?>
