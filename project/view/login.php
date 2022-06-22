03949b0557ac2762c5f521ea0758c8f6d4ec2ce4
uchsas <44012824+uchsas@users.noreply.github.com>
2022-06-22 20:30:46 +0600

    Update login.php

--- project/view/login.php
+++ project/view/login.php
@@ -1,20 +1,6 @@
-  <?php
+ <?php
  include('../controller/logincontroller.php')
-	   ?> 
-
-<html>
-<head>
-<style>
-
-</style>
-	<title>Login</title>
-	
-</head>
-<body>
-<body background="cat.jpg">
-<h1 style="background-color:WHITE;text-align:center">Welcome To Pet Shop</h1>
-
-
+	   ?>  
 <html>
     <head>
 	<title>Log in</title>
@@ -22,40 +8,36 @@
 	     <style>
 	            .login-div
 				{
-					background-color: yellow;
+				background-color: yellow;	
 				border:1px solid black;
 				margin:auto;
 				width:20%;
 				margin-top:17%;
 				
 				}
-				
  		 </style>
 	</head>
 	<body>
+	<body background="cat.jpg">
+<h1 style="background-color:WHITE;text-align:center">Welcome To Pet Shop</h1>
 	<div class="login-div">
 	 <fieldset>
         <legend align="center"><h1>Login</h1></legend>
 	   
 	   <form action="" method="post">
 	       <table align="center">
-		        <table>
-        <tr>
-          <td>Username:</td>
-          <td><input type="text" name="user" placeholder="Enter name Here"></td>
-				<span class="error"><?php echo $Usernameerr;?></span>
-        </tr>
-        <tr>
-          <td>Password:</td>
-          <td><input type="password" name="user_pass" placeholder="Enter Password Here"></td>
-        				<span class="error"><?php echo $passworderr;?></span>
-
-		</tr>
-        
-        
-      </table>
+		        <tr>
+				     <td><span><b>Username<b></span></td>
+					<td>:<input type="text" name="uname" value="<?php echo $uname;?>" placeholder="Username">
+						<span><?php echo $err_uname;?></span></td>
+			    </tr>
 				<tr>
-				<td><input type="submit" name="login" value="Login"><a href="homepage.php"></td>
+				     <td><span><b>Password</b></span></td>
+					<td>:<input type="password" value="<?php echo $pass;?>" name="pass" placeholder="Password">
+					<span><?php echo $err_pass;?></span></td>
+				</tr>
+				<tr>
+				<td><input type="submit" name="login" value="Login"></td>
 				</tr>
 		      </table>
 			  </form>
